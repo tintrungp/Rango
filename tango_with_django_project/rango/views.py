dbc17bb368d656a9a09d827e4a4f3fd552ee7fb7
Tin-Trung Pham <tintrungpham1@gmail.com>
2024-07-15 22:18:45 -0700

    Template Tags: Need to learn more, but good progress.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -19,10 +19,10 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {
-        'aboldmessage': 'This tutorial has been put together by Tin-Trung Pham.'
-    }
-    return render(request, 'rango/about.html', context=context_dict)
+    print(request.method)
+
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     context_dict = {}
