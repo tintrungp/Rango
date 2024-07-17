c0940fd11fcea1a3d752d253e91229a674fd223e
Tin-Trung Pham <tintrungpham1@gmail.com>
2024-07-17 10:47:42 -0700

    Creating forms + models for users

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,6 +1,8 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
 import re
+from django.contrib.auth.models import User
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -36,3 +38,14 @@ class PageForm(forms.ModelForm):
 
         exclude = ('category',)
 
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
